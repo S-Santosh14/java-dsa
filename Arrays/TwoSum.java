885da3c118aa72961a98465c6f341f4f4e11f917
Siddamsetti Santosh <santoshsiddamsetti0507@gmail.com>
2026-06-02 20:14:26 +0530

    Day 2: Solved Two Sum

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> h=new HashMap<>();
+        int ans[]=new int[2];
+        for(int i=0;i<nums.length;i++)
+        {
+            if(h.containsKey(target-nums[i]))
+            {
+                ans[0]=i;
+                ans[1]=h.get(target-nums[i]);
+            }
+            h.put(nums[i],i);
+        }
+        return ans;
+    }
+}
